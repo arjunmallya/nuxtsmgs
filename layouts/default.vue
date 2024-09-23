5fc60817e74fbfa4f450f21e5dbb897d9fd7f398
Arjun Mallya <arjunmallya796@gmail.com>
2024-09-24 01:36:47 +0530

    whatsapp button mb removed

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@
     <a
       href="https://wa.me/9739429439?text=Hello,%20I%20want%20to%20know%20about%20your%20services.&type=phone_number&app_absent=1"
       class="contact-button"
-      style="background-color: #25d366; bottom: 100px"
+      style="background-color: #25d366"
       target="_blank"
     >
       <img src="/imgs/whatsapp.png" alt="WhatsApp" />
